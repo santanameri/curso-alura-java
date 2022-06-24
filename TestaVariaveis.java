b35253dfe0275ed10331e5dd997027fcb3fc2973
Merielly Santana <80992709+santanameri@users.noreply.github.com>
2022-06-24 19:46:37 -0300

    atribuicao-variaveis

---
+++ TestaVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
