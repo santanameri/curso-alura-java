5224fb2fbd50bc5d11d12831cb2914cbe90f4802
Merielly Santana <80992709+santanameri@users.noreply.github.com>
2022-06-24 19:40:48 -0300

    quantidade-pessoas-atual

---
+++ TestaCondicional2.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional2 {
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+		int idade = 20;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 2;
+		
+		System.out.println("valor de acompanhado = " + acompanhado);
+		if(idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+			System.out.println("infelizmente você não pode entrar");
+			}
+		}
+	}
