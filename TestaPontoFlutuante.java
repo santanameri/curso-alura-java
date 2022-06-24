f37bf596842706aefdb006b84fc3e224d3d7b1bb
Merielly Santana <80992709+santanameri@users.noreply.github.com>
2022-06-24 19:44:57 -0300

    ponto-flutuante-divisao

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salário é " + salario);
+		
+		double idade = 37;
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println(novaTentativa);
+		
+	}
+}
